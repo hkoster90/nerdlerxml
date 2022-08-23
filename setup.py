7fb261466b09b5eab9ce5d7e6ce184dc56a62b07
Hendrik Koester <hkoster90@sapo.pt>
2022-08-23 23:38:55 +0100

    1.0.1

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ import setuptools
 
 setuptools.setup(
     name="nerdlerxml",
-    version="1.0.0",
+    version="1.0.1",
     author="Hendrik Koester",
     description="Converts XML to a dictionary. Can take inputs from URL, a file and binary data",
+    long_description="Converts XML to a dictionary. Can take inputs from URL, a file and binary data",
+    url="https://github.com/nerdlertech/nerdlerxml",
     packages=["nerdlerxml"]
 )
